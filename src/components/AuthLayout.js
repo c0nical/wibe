f618a013db8d16f2b8958e21e736cfb42f60d683
c0nical <124338703+c0nical@users.noreply.github.com>
2025-02-13 21:32:39 +0500

    v3
    img fix

--- src/components/AuthLayout.js
+++ src/components/AuthLayout.js
@@ -8,7 +8,7 @@ const AuthLayout = () => {
       <VideoBackground />
       <div className="z-10 bg-white bg-opacity-50 p-8 border-2 backdrop-blur-sm rounded-lg">
       <div className="flex flex-1 align-center justify-center p-6">
-          <img src="/assets/img/logo/logo.png" className="w-32" alt="Logo" />
+          <img src="./assets/img/logo/logo.png" className="w-32" alt="Logo" />
         </div>
         <Outlet /> {/* Здесь будут отображаться Login и SignUp */}
       </div>
